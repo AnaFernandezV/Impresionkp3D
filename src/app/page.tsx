5e1c58a01c0ed0a11b014199a9b2692b142db681
AnaFernandezV <amfervallejos@gmail.com>
2024-05-24 18:09:25 -0400

    Created component Header, Created Menu structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,28 @@
 "use client";
 import React from 'react';
-import { Parallax, ParallaxLayer, IParallax } from '@react-spring/parallax'
+import { Menu } from '../components/Header/Menu/Menu';
+import { Parallax, ParallaxLayer, IParallax } from '@react-spring/parallax';
+
 export default function Home() {
   return (
     <div >
-    <Parallax pages ={3}>    
+    <Parallax pages ={3}> 
+    <Menu/>   
       <ParallaxLayer 
       offset={0.1}
-      speed={1}
-      > 
+      speed={1}> 
+
       </ParallaxLayer>
 
-      <ParallaxLayer offset={0.99} speed={0.9}
-      
-      >
+      <ParallaxLayer offset={0.99} speed={0.9}>
         
       </ParallaxLayer>
-     
+    
         <ParallaxLayer style={{marginTop:'10%'}} offset={0.9999} speed={1.2}>
           
         </ParallaxLayer>
         <ParallaxLayer></ParallaxLayer>
-       
+      
       
 
     </Parallax>
---
+++ src/components/Header/Menu/Menu.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import '../Menu/menu.sass'
+const Menu: React.FC = () =>{
+
+    return(
+        <div className='menu'>
+            <ul>
+                <li>Servicios
+
+
+                </li>
+                <li>Galería
+
+
+                </li>
+                <li>Contacto
+
+
+                </li>
+
+            </ul>
+
+        </div>
+
+
+);
+}
+export { Menu };
