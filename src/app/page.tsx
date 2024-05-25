a5c79fe37e342a304664a2114059f20cfef10492
AnaFernandezV <amfervallejos@gmail.com>
2024-05-24 23:15:29 -0400

    Folder Assets created,folder Icons and Images created,added parallax images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,39 @@
 "use client";
 import React from 'react';
 import { Menu } from '../components/Header/Menu/Menu';
-import { Parallax, ParallaxLayer, IParallax } from '@react-spring/parallax';
+import backgroundCube from '../assets/Images/cube-background.png';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 
 export default function Home() {
   return (
     <div >
-    <Parallax pages ={3}> 
-    <Menu/>   
-      <ParallaxLayer 
-      offset={0.1}
-      speed={1}> 
+      <Parallax pages ={3}> 
+      <Menu/>   
+        <ParallaxLayer 
+        offset={0.1}
+        speed={1}
+        style={{
+          backgroundImage:`url(${backgroundCube.src})`,
+          backgroundSize:'cover',
+          top:0,
+          zIndex:'-1',
+          marginTop:'-70px',
+        }}> 
+      
+        </ParallaxLayer>
 
-      </ParallaxLayer>
-
-      <ParallaxLayer offset={0.99} speed={0.9}>
-        
-      </ParallaxLayer>
-    
-        <ParallaxLayer style={{marginTop:'10%'}} offset={0.9999} speed={1.2}>
+        <ParallaxLayer offset={0.99} speed={0.9}>
           
         </ParallaxLayer>
-        <ParallaxLayer></ParallaxLayer>
       
-      
+          <ParallaxLayer style={{marginTop:'10%'}} offset={0.9999} speed={1.2}>
+        {/*   <img src={cubo3} style={{ display: 'block', width: '100%', marginLeft: '0%' ,marginTop:'-50%',zIndex:'-1',position:'relative',}} /> */}
+          </ParallaxLayer>
+          <ParallaxLayer></ParallaxLayer>
+        
+        
 
-    </Parallax>
+      </Parallax>
     </div>
   );
 }
