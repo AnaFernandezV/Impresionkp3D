176abfb7d61f2f110316cccd3041800dd05c4af1
AnaFernandezV <amfervallejos@gmail.com>
2024-06-12 18:28:47 -0400

    animated text content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,6 @@
 import React , { useRef } from 'react';
 import '@/components/Header/header.sass';
 import { Parallax, ParallaxLayer, IParallax } from '@react-spring/parallax';
-import backgoundLineal from '@/assets/Images/background-lineal.png';
-import bigCube from '@/assets/Images/big-cube.png';
-import someCubes from '@/assets/Images/some-cubes.png';
 import { Menu } from '@/components/Header/Menu/Menu';
 import { Content } from '@/components/Content/Content';
 import { Service } from '@/components/Services/Service';
@@ -19,34 +16,24 @@ export default function Home() {
   return (
       <>
       <Parallax ref={parallax} pages ={3}> 
+
       <ParallaxLayer offset={0} >
         <div className='header'>
           <Logo/>   
           <Menu/>
         </div>
-      </ParallaxLayer>    
-        <ParallaxLayer 
-        offset={0.1}
-        speed={1}> 
-      
-        </ParallaxLayer>
+      </ParallaxLayer>  
 
-        <ParallaxLayer 
+          <ParallaxLayer 
           offset={0.1}
           speed={1}> 
 
           <ParallaxLayer speed={2} onClick={() => parallax.current.scrollTo(1)} > 
             <Content />
           </ParallaxLayer>
-          
-      
-          {/* <img src={bigCube.src} style={{ display: 'block', width: '100%', marginLeft: '0%' ,marginTop:'0%',zIndex:'-1',position:'relative',}} />
- */}
+        
       </ParallaxLayer>
-      <ParallaxLayer offset={0.99} speed={0.9}
-        >
-        </ParallaxLayer>
-      
+        
           <ParallaxLayer style={{marginTop:'0%',backgroundColor:"#FFF",}} offset={1} speed={1.2} onClick={() => parallax.current.scrollTo(1.6)}>
             <Service/>
 
--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -2,17 +2,38 @@ import React from "react";
 import './Content.sass';
 import '../ButtonCotizar/Button.sass';
 import { ButtonCotizar } from "../ButtonCotizar/Button";
+import { motion } from "framer-motion";
+
 
 
 const Content: React.FC = () =>{
-
+  const container = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        ease: [0.42, 0, 0.58, 1], 
+        duration: 1, 
+        staggerChildren: 0.2 
+      }
+    }
+  }
+  const item = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1 }
+  }
     return(
-        <div className='index' >
-        <h1>¡Da vida a tus ideas con nuestras impresiones <span style={{color:'#52ffc5',}}>3D</span>!</h1>
-        <h2>Descubre una nueva dimensión en creatividad.<hr></hr>Cotiza tus proyectos hoy mismo.</h2>
+        <motion.div 
+        variants={container}
+        initial="hidden"
+        animate="show"
+        className='index' >
         
-            <ButtonCotizar/>
-        </div>
+        <motion.h1 variants={item}>¡Da vida a tus ideas con nuestras impresiones <span style={{color:'#52ffc5',}}>3D</span>!</motion.h1>
+        <motion.h2 variants={item}>Descubre una nueva dimensión en creatividad.<hr></hr>Cotiza tus proyectos hoy mismo.</motion.h2>
+        
+          <ButtonCotizar/>
+        </motion.div>
     );
 }
 export { Content };
