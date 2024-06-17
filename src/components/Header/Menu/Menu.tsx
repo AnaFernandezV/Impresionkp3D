e213d8b45f0b01ce166dfe9f7fe1fcdfd4a5b2ce
AnaFernandezV <amfervallejos@gmail.com>
2024-06-17 16:43:10 -0400

    nav page created

--- src/components/Header/Menu/Menu.tsx
+++ src/components/Header/Menu/Menu.tsx
@@ -8,8 +8,9 @@ const Menu:React.FC = () =>{
             
             <ul>
                 <a href=""><li>Servicios</li></a>
+                <a href=""><li>Nuestros Trabajos</li></a>
                 <a href=""><li>Galería</li></a>
-                <a href=""><li>Contacto</li></a>
+                <a href="https://wa.me/56972589359" target="_blank"><li>Contacto</li></a>
                 
             </ul>
         </div>
